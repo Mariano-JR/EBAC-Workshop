eaee0a58cbdcf70c51c98b97409ff87050b291b1
Mariano-JR <mariano_br123@hotmail.com>
2022-08-10 21:19:29 -0300

    repositorio do desafio workshop ebac reactjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        
+        <div className='header'>
+          <h2>Meus Livros</h2>
+        </div>
+
+        <div className='search'>
+          <input type="search" placeholder="Procure Aqui..."></input>
+        </div>
+
+        <div className='books'>
+          <div>
+            <img src='https://altabooks.com.br/wp-content/uploads/2021/07/Codigo_Limpo-scaled.jpg' height='250px'></img>
+            <p>NOTA: 5/5</p>
+          </div>
+
+          <div>
+            <img src='https://altabooks.com.br/wp-content/uploads/2021/07/Codigo_Limpo-scaled.jpg' height='250px'></img>
+            <p>NOTA: 5/5</p>
+          </div>
+
+          <div>
+            <img src='https://altabooks.com.br/wp-content/uploads/2021/07/Codigo_Limpo-scaled.jpg' height='250px'></img>
+            <p>NOTA: 5/5</p>
+          </div>
+
+          <div>
+            <img src='https://altabooks.com.br/wp-content/uploads/2021/07/Codigo_Limpo-scaled.jpg' height='250px'></img>
+            <p>NOTA: 5/5</p>
+          </div>
+        </div>
+
+        <div className='footer'>
+          <h4>FEITO POR MARIANO JUNIOR</h4>
+        </div>
+      </div>
   );
 }
 
